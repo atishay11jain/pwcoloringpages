ecc18bfaf7e9d7430008e99f60ed3dd99838c105
atishay11jain <88791784+atishay11jain@users.noreply.github.com>
2025-03-21 20:57:34 +0530

    assets folder created

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+$(document).on('click', '#connectbtn', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $('#productsSection').offset().top - 100
+    }, 100);        
+});
+
+$(document).ready(function () {
+    if (history.state?.clicked === "connectbtn") {
+        $('html, body').animate({
+            scrollTop: $('#productsSection').offset().top - 100
+        }, 100);
+    }
+});
---
+++ assets/js/productDetails.js
@@ -0,0 +1,57 @@
+$(document).on("click", ".dropdown-item", function () {
+  var selectedFormat = $(this).find("span[data-format]").text();
+  $("#dropdownMenuButton .inner").text(selectedFormat);
+  $(".dropdown-item").removeClass("active");
+  $(this).addClass("active");
+  removeErrorMessages('error-message');
+
+});
+function removeErrorMessages(elementClassName) {
+  let existingError = document.getElementsByClassName(elementClassName)[0];
+  if (existingError) {
+      existingError.remove();
+  }
+}
+function showError(message, elementClassName) {
+  let parentElement = document.getElementsByClassName(elementClassName)[0];
+
+  removeErrorMessages('error-message'); // Remove any existing error messages
+
+  // Create a new span element
+  let errorSpan = document.createElement('div');
+  errorSpan.classList.add('error-message'); // Add a class for styling
+  errorSpan.textContent = message; // Set error message
+
+  // Apply styling (optional)
+  errorSpan.style.color = 'red';
+  errorSpan.style.fontSize = '12px';
+  errorSpan.style.marginLeft = '10px';
+  errorSpan.style.marginBottom = '10px';
+
+  // Append the error message after the element
+  parentElement.insertAdjacentElement('afterend', errorSpan);
+}
+
+$(document).on("click", "#downloadButton", function (e) {
+  e.preventDefault(); // Prevent default link behavior
+  var selectedFormat = $("#dropdownMenuButton .inner").text();
+  if(selectedFormat === "Format"){
+    showError("Please Select A Format", "product-details-download-section");
+    return;
+  }
+  let imageUrl = "images/Home_Img_1"; // Update with actual image URL
+
+  let link = document.createElement("a");
+  link.href = imageUrl + "." + selectedFormat; // Append format
+  link.download = "downloaded-file." + selectedFormat; // Set download filename
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+$(document).on('click', '#connectbtn', function (e) {
+  e.preventDefault();
+  history.pushState({ clicked: "connectbtn" }, "", "/");
+  window.location.href = "/";
+});
+
