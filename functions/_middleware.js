7c1df11553865edfe439ddb5d2a0e78f1d57ae01
atishay11jain <88791784+atishay11jain@users.noreply.github.com>
2025-03-22 09:04:52 +0000

    middleware updated

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,35 +1,21 @@
 export async function onRequest(context) {
     let url = new URL(context.request.url);
 
-    // If user visits "/free-coloring-pages", serve "coloring.html"
     if (url.pathname === "/free-coloring-pages") {
-        return new Response(
-            await (await fetch(context.env.ASSETS.fetch("coloring-pages.html"))).text(), 
-            { headers: { "Content-Type": "text/html" } }
-        );
+        return context.env.ASSETS.fetch("coloring-pages.html");
     }
 
     if (url.pathname === "/unicorn-coloring-pages") {
-        return new Response(
-            await (await fetch(context.env.ASSETS.fetch("unicorn.html"))).text(), 
-            { headers: { "Content-Type": "text/html" } }
-        );
+        return context.env.ASSETS.fetch("unicorn.html");
     }
 
     if (url.pathname === "/mermaid-coloring-pages") {
-        return new Response(
-            await (await fetch(context.env.ASSETS.fetch("mermaid.html"))).text(), 
-            { headers: { "Content-Type": "text/html" } }
-        );
+        return context.env.ASSETS.fetch("mermaid.html");
     }
 
     if (url.pathname === "/butterfly-coloring-pages") {
-        return new Response(
-            await (await fetch(context.env.ASSETS.fetch("butterfly.html"))).text(), 
-            { headers: { "Content-Type": "text/html" } }
-        );
+        return context.env.ASSETS.fetch("butterfly.html");
     }
 
-    // Continue serving other pages normally
     return context.next();
 }
